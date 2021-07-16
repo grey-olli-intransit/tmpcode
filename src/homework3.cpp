7ea293a9530b8d9c7480a4844fc0e2e9b60c6db0
NoName <nomail@nodomain.localdomain>
2021-07-16 23:52:38 +0300

    fix

--- src/homework3.cpp
+++ src/homework3.cpp
@@ -1,26 +1,37 @@
 #include <iostream>
 #include <string>
 
-void to_binary(int x, std::string * result) {
+void to_binary(int x, std::string & result) {
     int y=x/2;
+
+    if (x==0) {
+        result.insert(0,"0");
+	std::cout << " Это число в двоичном представлении:" << std::endl << " " << result << std::endl;
+        return;
+    }
+
+    if (x==1) {
+        result.insert(0,"1");
+	std::cout << " Это число в двоичном представлении:" << std::endl << " " << result << std::endl;
+        return;
+    }
+
     if (y==1) {
-	(*result).insert(0,std::__cxx11::to_string(x%2));
-        (*result).insert(0,"1");
-	std::cout << " " << x << " в двоичном представлении:" << std::endl << " " << *result << std::endl;
+	result.insert(0,std::__cxx11::to_string(x%2));
+        result.insert(0,"1");
+	std::cout << " Это число в двоичном представлении:" << std::endl << " " << result << std::endl;
 	return;
     }
     else {
-        (*result).insert(0,std::__cxx11::to_string(x%2));
+        result.insert(0,std::__cxx11::to_string(x%2));
 	to_binary(y, result);
     }
 }
 
 int power(int a, int b) {
     int power=1;
-    while(b!=0) {
+    while(b--) 
       power*=a;
-      b--;
-    }
     return power;
 }
 
@@ -37,7 +48,7 @@ int main(int argc, char ** argv) {
     std::cout << "Введите целое число: ";
     std::cin >> x;
 
-    to_binary(x, &result);
+    to_binary(x, result);
 
     std::cout << "Введите целое число (основание): ";
     std::cin >> x;
